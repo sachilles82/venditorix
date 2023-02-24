02ed33885c197b342b743f9b6fda3c90316d1dad
sachilles <kina98@gmx.ch>
2023-02-24 14:53:01 +0100

    Turbo Links für Jetstream App erstellt

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import '../js/libs/stimulus';
+import '../js/libs/turbo';
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
